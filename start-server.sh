63f6347356875d24b4023450ff20e85fa849d86f
Ekkehard Petzold <mail@programmator.de>
2025-10-11 03:21:32 +0200

    Ignoring start-server.sh

---
+++ start-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Simple script to start local development server
+echo "Starting local development server..."
+echo "Open your browser to: http://localhost:8000"
+echo "Press Ctrl+C to stop the server"
+echo ""
+python3 -m http.server 8000
+
